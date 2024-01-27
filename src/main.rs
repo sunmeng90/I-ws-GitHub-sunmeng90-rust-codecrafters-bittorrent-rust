1ce88bd0c8bc1e76e0bd84cdb42561ae99c637f7
Meng <sunmeng90@126.com>
2024-01-27 23:26:15 +0800

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,23 @@ use serde_json;
 use std::env;
 
 // Available if you need it!
-// use serde_bencode
+use serde_bencode;
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
+    let first_char = encoded_value.chars().next().unwrap();
+    if first_char.is_digit(10) {
         // Example: "5:hello" -> "hello"
         let colon_index = encoded_value.find(':').unwrap();
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
         return serde_json::Value::String(string.to_string());
-    } else {
+    } else if first_char == 'i' {
+        return serde_json::Value::Number(serde_bencode::from_str(encoded_value).unwrap())
+    }
+    {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
 }
