51d944be41ae64b8b6a42678479a891b0cbbf0a2
Meng <sunmeng90@126.com>
2024-02-02 23:57:35 +0800

    pass 4th stage

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
+use std::collections::{BTreeMap, HashMap};
 use serde_json;
 use std::env;
+use std::iter::Map;
 
 // Available if you need it!
 use serde_bencode;
+use serde_json::Value;
 
 enum Bencode {
     String(String),
     Integer(i64),
     List(Vec<Bencode>),
+    Dict(BTreeMap<Vec<u8>, Bencode>),
 }
 
 #[allow(dead_code)]
@@ -42,6 +46,24 @@ fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
                 rest = remainder;
             }
             return (values.into(), &rest[1..]);
+        }
+        // d3:cow3:moo4:spam4:eggse
+        Some('d') => {
+            let mut dict: serde_json::Map<String, Value> = serde_json::Map::new();
+            let mut rest: &str = encoded_value
+                .split_at(1)
+                .1;
+
+            loop {
+                if rest.starts_with('e') {
+                    break;
+                }
+                let (key, remainder) = decode_bencoded_value(rest);
+                let (value, remainder) = decode_bencoded_value(remainder);
+                dict.insert(key.to_string().into(), value);
+                rest = remainder;
+            }
+            return (dict.into(), &rest[1..]);
         }
         _ => {}
     }
